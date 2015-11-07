23298e21a18b1f89f1b6516f3db0b735f0d31b3a
kolbeypruitt <kolbeypruitt@gmail.com>
2015-11-06 17:25:34 -0700

    up-votes and down-votes work

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,8 @@ app.controller("PostController", function($scope){
   $scope.allPosts = [];
   $scope.newPost = function () {
     var d = new Date();
+    $scope.postObj.votes = 0;
+    $scope.showNewPost = false;
     $scope.postObj.rightNow = d.getTime();
     $scope.allPosts.push($scope.postObj);
     $scope.postObj = {};
@@ -12,9 +14,16 @@ app.controller("PostController", function($scope){
     var d = new Date();
     $scope.postObj.rightNow = d.getTime();
   }
-  // $scope.vote = function (direction) {
-  //   if (direction==='up') {
 
-  //   }
+  // $scope.addComment = function () {
+  //   $scope.post.
   // }
+  $scope.vote = function (direction) {
+    if (direction==='up') {
+      this.post.votes += 1;
+    } else if (direction==='down') {
+      this.post.votes -= 1;
+    }
+  }
+
 });
